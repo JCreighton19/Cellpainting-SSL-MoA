89a7e0881697a8a34c52addd8a5b5d301e22d1ca
John <120530843+JCreighton19@users.noreply.github.com>
2026-05-21 14:53:25 -0400

    Changed dataset schema again - now, each row still refers to a single plate/well/site combo and there are 5 separate columns for each of the 5 images (one per channel)

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -23,15 +23,29 @@ class CellPaintingDataset(Dataset):
         self.channels = channels if channels is not None else [1,2,3,4,5]
         self.tile_size = tile_size
 
+        self._cache = {}
+
     def __len__(self):
         return len(self.metadata)
 
     def __getitem__(self, idx):
         for _ in range(10):
-            row = self.metadata.iloc[random.randint(0, len(self.metadata) - 1)]
-            image = tiff.imread(str(row["image_path"])).astype(np.float32)
-            if image.ndim == 2:
-                image = image[None, :, :]
+            row = self.metadata.iloc[idx]
+            paths = [
+                row["url_origdna"],
+                row["url_origagp"],
+                row["url_origmito"],
+                row["url_origer"],
+                row["url_origrna"],
+            ]
+
+            channels = []
+            for p in paths:
+                if p not in self._cache:
+                    self._cache[p] = tiff.imread(str(p)).astype(np.float32)
+                channels.append(self._cache[p])
+
+            image = np.stack(channels, axis=0)
 
             image = self._normalize_channels(image)
             C, H, W = image.shape
@@ -71,7 +85,7 @@ class CellPaintingDataset(Dataset):
         return normed
 
     def _is_informative(self, tile):
-        signal = np.percentile(tile[0], 95)  # use DNA channel only
-        variance = tile.var()
+        signal = np.percentile(tile[4], 95) # channel 5=DNA
+        variance = tile[4].var()
 
         return signal > 0.02 and variance > 0.005
