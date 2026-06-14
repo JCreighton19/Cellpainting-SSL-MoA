83d9738e3fa153d198a85003152fd8a09a3a71ab
John <120530843+JCreighton19@users.noreply.github.com>
2026-06-14 13:36:12 -0400

    Changed train_dino.slurm to be self-managing and automatically restart after reaching time limit. Uses latest checkpoint from current training loop to ensure continued learning

--- models/config.py
+++ models/config.py
@@ -2,7 +2,7 @@
 
 CONFIG = {
     "lr": 5e-4,
-    "n_epochs": 60,
+    "n_epochs": 200,
     "batch_size": 32, # GPU limited to 32 without gradient accumulation
     "num_workers": 8,
     "weight_decay": 0.04,
